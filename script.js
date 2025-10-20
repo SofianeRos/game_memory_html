7ed258512b7aad9696fb0f06d27d51dfc77a53d3
SofianeRos <sosoros66@icloud.com>
2025-10-20 11:30:18 +0200

    recuperation des elements dans le dom

--- script.js
+++ script.js
@@ -9,12 +9,21 @@ console.log("Hello World");
 
 
 
-function handlerDOMContentLoaded(){
-    // code du jeu //
-   
+function handlerDOMContentLoaded() {
+
+
     console.log("Le jeu demarre ");
- 
-}   
+
+    // on recupere dans le dom les elements dont on a besoin
+
+    const elHiScore = document.getElementById('hi-score');
+    const elBtnResetScore = document.getElementById('btn-reset-score');
+    const elCurrentScore = document.getElementById('current-score');
+    const elDeck = document.getElementById('deck');
+    const elFinalScore = document.getElementById('final-score');
+    const elBtnPlayAgain = document.getElementById('btn-play-again');
+    const elModalWin = document.getElementById('modal-win');
+}
 
 // mise en place d'un ecouteur pour lancer ne lancer le code que lorsque le navigateur a fini de charger le DOM
-document.addEventListener('DOMContentLoaded', handlerDOMContentLoaded );
+document.addEventListener('DOMContentLoaded', handlerDOMContentLoaded);
